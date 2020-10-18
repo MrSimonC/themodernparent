e40ae4056ccf03b0a9a61c079af9ee108f7602ef
Simon Crouch <simon.crouch@felinesoft.com>
2020-10-18 23:21:17 +0100

    regig namespaces

--- Client/Program.cs
+++ Client/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace TheModernParent.Client
+namespace TheModernParent
 {
     public class Program
     {
