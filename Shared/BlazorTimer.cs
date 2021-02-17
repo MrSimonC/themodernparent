9862b72e20f9932f5e1920d780cc48e7b78f384e
Simon Crouch <simon.crouch@felinesoft.com>
2021-02-17 23:09:15 +0000

    experimentation wip

---
+++ Shared/BlazorTimer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Timers;
+
+namespace TheModernParent.Shared
+{
+    // https://wellsb.com/csharp/aspnet/blazor-timer-navigate-programmatically/
+
+    public sealed class BlazorTimer
+    {
+        private Timer _timer;
+
+        public void SetTimer(double intervalMilliseconds, bool repeat)
+        {
+            _timer = new Timer(intervalMilliseconds);
+            _timer.Elapsed += NotifyTimerElapsed;
+            _timer.AutoReset = repeat;
+            _timer.Enabled = true;
+        }
+
+        public event Action OnElapsed;
+
+        private void NotifyTimerElapsed(object source, ElapsedEventArgs e)
+        {
+            OnElapsed?.Invoke();
+            if (!_timer.AutoReset)
+            {
+                _timer.Stop();
+                _timer.Dispose();
+                _timer = null;
+            }
+        }
+    }
+}
